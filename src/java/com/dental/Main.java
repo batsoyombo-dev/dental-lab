e0367dc5d09d0bd50cbf75b0d9c955c8215dbe78
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-04-04 20:41:22 +0800

    main change

--- src/java/com/dental/Main.java
+++ src/java/com/dental/Main.java
@@ -1,6 +1,5 @@
 package com.dental;
 
-<<<<<<< HEAD
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -20,12 +19,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-=======
-public class Main {
-
-    public static void main(String[] args) {
-
->>>>>>> origin/main
     }
 
 }
