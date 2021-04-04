9785307a02a0555e6fb5b81bc134f979dbfd6746
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-04-04 17:56:18 +0800

    initial commit

---
+++ src/java/com/dental/Main.java
@@ -0,0 +1,24 @@
+package com.dental;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(super.getClass().getResource("/controllers/home_layout.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Dental Lab");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/java/com/dental/controllers/HomeController.java
@@ -0,0 +1,4 @@
+package com.dental.controllers;
+
+public class HomeController {
+}
