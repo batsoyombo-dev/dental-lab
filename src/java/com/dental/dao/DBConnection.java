1f2c4e3fbda1c4002d5d166c50c5e5f498cdf079
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-04-14 13:36:54 +0800

    rollback

--- src/java/com/dental/dao/DBConnection.java
+++ src/java/com/dental/dao/DBConnection.java
@@ -1,10 +1,5 @@
 package com.dental.dao;
 
-import com.dental.models.Doctor;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
-
 import java.sql.SQLException;
 
 public class DBConnection {
@@ -12,9 +7,5 @@ public class DBConnection {
     public static String USERNAME = "super";
     public static String PASSWORD = "";
     public static String URL = "jdbc:mysql://localhost:3306/dental?user=super";
-
-    public static ConnectionSource getConnectionSource() throws SQLException {
-        return new JdbcConnectionSource(URL);
-    }
 
 }
