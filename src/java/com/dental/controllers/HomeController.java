1bcc04e2e8b87c03d9454ee8c2033aaef45f4a6f
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-04-23 18:47:04 +0800

    navigation chagnge commit

--- src/java/com/dental/controllers/HomeController.java
+++ src/java/com/dental/controllers/HomeController.java
@@ -40,88 +40,63 @@ public class HomeController implements Initializable {
 
     public void inflateScreen(String screen) throws IOException {
         String path;
+        if (this.currentScreen == null || !this.currentScreen.equals(screen))
+            this.currentScreen = screen;
+        else return;
         switch(screen) {
             case "register" -> {
-                if(this.navigateable instanceof RegisterScreenController)
-                    return;
                 this.navigateable = new RegisterScreenController();
                 path = "/screen/register_screen_layout.fxml";
             }
             case "search-entries" -> {
-                if(this.navigateable instanceof SearchEntriesScreenController)
-                    return;
                 this.navigateable = new SearchEntriesScreenController();
                 path = "/screen/search_entries_screen_layout.fxml";
             }
             case "analysis" -> {
-                if(this.navigateable instanceof AnalysisScreenController)
-                    return;
                 this.navigateable = new AnalysisScreenController();
                 path = "/screen/analysis_screen_layout.fxml";
             }
             case "gen-invoice" -> {
-                if(this.navigateable instanceof GenerateInvoiceScreenController)
-                    return;
                 this.navigateable = new GenerateInvoiceScreenController();
                 path = "/screen/generate_invoice_screen_layout.fxml";
             }
             case "daily-expenses" -> {
-                if(this.navigateable instanceof DailyExpensesScreenController)
-                    return;
                 this.navigateable = new DailyExpensesScreenController();
                 path = "/screen/daily_expenses_screen_layout.fxml";
             }
             case "today-delivery" -> {
-                if(this.navigateable instanceof TodayDeliveryScreenController)
-                    return;
                 this.navigateable = new TodayDeliveryScreenController();
                 path = "/screen/today_delivery_screen_layout.fxml";
             }
             case "ledger-account" -> {
-                if(this.navigateable instanceof LedgerAccountScreenController)
-                    return;
                 this.navigateable = new LedgerAccountScreenController();
                 path = "/screen/ledger_account_screen_layout.fxml";
             }
             case "doctor-profile" -> {
-                if(this.navigateable instanceof DoctorProfileScreenController)
-                    return;
                 this.navigateable = new DoctorProfileScreenController();
                 path = "/screen/doctor_profile_screen_layout.fxml";
             }
             case "work-type" -> {
-                if(this.navigateable instanceof WorkTypeScreenController)
-                    return;
                 this.navigateable = new WorkTypeScreenController();
                 path = "/screen/work_type_screen_layout.fxml";
             }
             case "supplier-profile" -> {
-                if(this.navigateable instanceof SupplierProfileScreenController)
-                    return;
                 this.navigateable = new SupplierProfileScreenController();
                 path = "/screen/supplier_profile_screen_layout.fxml";
             }
             case "employee-profile" -> {
-                if(this.navigateable instanceof EmployeeProfileScreenController)
-                    return;
                 this.navigateable = new EmployeeProfileScreenController();
                 path = "/screen/employee_profile_screen_layout.fxml";
             }
             case "add-lab-profile" -> {
-                if(this.navigateable instanceof AddLabProfileScreenController)
-                    return;
                 this.navigateable = new AddLabProfileScreenController();
                 path = "/screen/add_lab_profile_screen_layout.fxml";
             }
             case "add-user" -> {
-                if(this.navigateable instanceof AddUserScreenController)
-                    return;
                 this.navigateable = new AddUserScreenController();
                 path = "/screen/add_user_screen_layout.fxml";
             }
             case "records" -> {
-                if(this.navigateable instanceof RecordScreenController)
-                    return;
                 this.navigateable = new RecordScreenController();
                 path = "/screen/record_screen_layout.fxml";
             }
