4b11edf1e3131cc65d05c99ce8042faaa9e5c58c
Amraa9656 <amarjargalamraa61@gmail.com>
2021-05-02 17:45:59 +0800

    Hajidsvren

--- src/java/com/dental/controllers/screens/RecordScreenController.java
+++ src/java/com/dental/controllers/screens/RecordScreenController.java
@@ -1,14 +1,87 @@
 package com.dental.controllers.screens;
 
+import com.dental.dao.CrudDAO;
+import com.dental.models.Doctor;
+import com.dental.models.Employee;
+import com.dental.models.Registration;
 import com.dental.utils.Navigateable;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class RecordScreenController implements Initializable, Navigateable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private TableView<Doctor> DoctorTable;
 
+    @FXML
+    private TableColumn<Doctor, String> id;
+
+    @FXML
+    private TableColumn<Doctor, String> DoctorName;
+
+    @FXML
+    private TableColumn<Doctor, String> address;
+
+    @FXML
+    private TableColumn<Doctor, String> MobileNumber;
+
+    @FXML
+    private TableView<Employee> employeeTable;
+
+    @FXML
+    private TableColumn<Employee, String> SrNo;
+
+    @FXML
+    private TableColumn<Employee, String> name;
+
+    @FXML
+    private TableColumn<Employee, String> DOB;
+
+    @FXML
+    private TableColumn<Employee, String> Designation;
+
+    @FXML
+    private TableColumn<Employee, String> salary;
+
+    @FXML
+    private TableColumn<Employee, String> MobileNu;
+
+    private List<Doctor> DoctorList;
+    private List<Employee> employeeList;
+    private CrudDAO<Doctor> crudDAODoctor;
+    private CrudDAO<Employee> crudDAOEmployee;
+    public RecordScreenController()
+    {
+        DoctorList = new ArrayList<>();
+        employeeList = new ArrayList<>();
+        this.crudDAODoctor = new CrudDAO<>(Doctor.class);
+        this.crudDAOEmployee = new CrudDAO<>(Employee.class);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        this.id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.DoctorName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.address.setCellValueFactory(new PropertyValueFactory<>("clinicAddress"));
+        this.MobileNumber.setCellValueFactory(new PropertyValueFactory<>("mobileNumber"));
+        this.SrNo.setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.DOB.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        this.Designation.setCellValueFactory(new PropertyValueFactory<>("designation"));
+        this.salary.setCellValueFactory(new PropertyValueFactory<>("salaryMonth"));
+        this.MobileNu.setCellValueFactory(new PropertyValueFactory<>("mobileNumber"));
+        DoctorList = crudDAODoctor.list();
+        employeeList = crudDAOEmployee.list();
+        DoctorTable.getItems().addAll(DoctorList);
+        employeeTable.getItems().addAll(employeeList);
     }
 }
--- src/java/com/dental/controllers/screens/WorkTypeScreenController.java
+++ src/java/com/dental/controllers/screens/WorkTypeScreenController.java
@@ -1,14 +1,146 @@
 package com.dental.controllers.screens;
 
+import com.dental.dao.CrudDAO;
+import com.dental.models.WorkType;
 import com.dental.utils.Navigateable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import com.dental.models.WorkType;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 public class WorkTypeScreenController implements Initializable, Navigateable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    @FXML
+    private TextField WorkTypeText;
+
+    @FXML
+    private TextField ChargeText;
+
+
+
+
+    @FXML
+    private TableView<WorkType> WorkTypeWork;
+
+    @FXML
+    private TableColumn<WorkType, Float> Charge;
+
+    @FXML
+    private TableColumn<WorkType, String> srNo;
+
+    @FXML
+    private TableColumn<WorkType, String> WorkType;
+
+    private CrudDAO<WorkType> crudDAO;
+    private List<WorkType> workTypeList;
+    private String workType;
+    private float charge;
+    private WorkType workTypes ;
+    private WorkType updateWorkType;
+    private WorkType DeleteWorkType;
+
+
+    public WorkTypeScreenController()
+    {
+        crudDAO = new CrudDAO<>(WorkType.class);
+        workTypeList = new ArrayList<>();
+    }
+
+    @FXML
+    void handleClickCancel(ActionEvent event)
+    {
+        WorkTypeText.clear();
+        ChargeText.clear();
+    }
+
+    @FXML
+    void handleClickDelete(ActionEvent event)
+    {
+        if (check())
+        {
+            this.setValue();
+            DeleteWorkType = WorkTypeWork.getSelectionModel().getSelectedItem();
+            crudDAO.delete(DeleteWorkType.getId());
+            int index = WorkTypeWork.getSelectionModel().getSelectedIndex();
+            WorkTypeWork.getItems().remove(index);
+        }
+
+
+    }
+
+    @FXML
+    void handleClickSave(ActionEvent event)
+    {
+        setValue();
+        workTypes = new WorkType(UUID.randomUUID().toString(),this.workType,UUID.randomUUID().toString(),this.charge);
+        this.crudDAO.create(workTypes);
+        WorkTypeWork.getItems().clear();
+        workTypeList = crudDAO.list();
+        WorkTypeWork.getItems().addAll(workTypeList);
+        workType = WorkTypeText.getText();
+        charge = Float.parseFloat(ChargeText.getText());
+        WorkTypeText.clear();
+        ChargeText.clear();
+    }
+
+    @FXML
+    void handleClickUpdate(ActionEvent event)
+    {
+
+            this.setValue();
+            updateWorkType = WorkTypeWork.getSelectionModel().getSelectedItem();
+            updateWorkType.setWork(workType);
+            updateWorkType.setCharge(charge);
+            crudDAO.update(updateWorkType);
+            int index = WorkTypeWork.getSelectionModel().getSelectedIndex();
+            WorkTypeWork.getItems().set(index, updateWorkType);
+    }
+
+    private void setValue()
+    {
+        workType = WorkTypeText.getText();
+        charge = Float.parseFloat(ChargeText.getText());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        this.srNo.setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.WorkType.setCellValueFactory(new PropertyValueFactory<>("work"));
+        this.Charge.setCellValueFactory(new PropertyValueFactory<>("charge"));
+        workTypeList = crudDAO.list();
+        WorkTypeWork.getItems().addAll(workTypeList);
+    WorkTypeWork.getSelectionModel().selectedItemProperty().addListener((observable, a , b) ->
+    {
+        updateWorkType = WorkTypeWork.getSelectionModel().getSelectedItem();
+        if (updateWorkType == null)
+        {
+            return;
+        }
+        WorkTypeText.setText(updateWorkType.getWork());
+        ChargeText.setText(String.valueOf(updateWorkType.getCharge()));
+    });
+    }
+    private Boolean check()
+    {
+        if (WorkTypeText.getText().trim().isEmpty() || ChargeText.getText().trim().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Алдаа ");
+            alert.setHeaderText("Алдаа гарлаа ");
+            return false;
+        }
+        return true;
     }
 }
