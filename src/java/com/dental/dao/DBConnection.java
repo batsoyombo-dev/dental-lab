569a6740493be39719b95c5d54dfe98c7eb47bf2
batsoyombo-dev <batsoyombo.kh@gmail.com>
2021-05-03 20:03:44 +0800

    landing screen

--- src/java/com/dental/dao/DBConnection.java
+++ src/java/com/dental/dao/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 
     public static String USERNAME = "super";
     public static String PASSWORD = "";
-    public static String URL = "jdbc:mysql://localhost:3306/dental?user=super";
+    public static String URL = "jdbc:mysql://localhost:3306/dental?user=" + USERNAME;
 
     public static ConnectionSource getConnectionSource() throws SQLException {
         return new JdbcConnectionSource(URL);
